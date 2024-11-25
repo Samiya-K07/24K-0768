75e67f7faee22183d0b1e444366af3ac54b011ca
Samiya Khan <khansamiya2005@gmail.com>
2024-11-25 23:00:50 +0500

    Update Q7.c

---
+++ Lab 10 Tasks/Q7.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define METER_TO_KM 0.001  
+
+
+void conversion(float m) 
+{
+    static int count = 0;  
+    count++; 
+
+    float km = m * METER_TO_KM;
+    
+    printf("Conversion: %.2f meters = %.2f kilometers\n", m, km);
+    printf("This function has been called %d times!!\n", count);
+}
+
+int main () {
+    
+    float m;
+    char choice;
+    printf("Conversion factor: 1 meter = %.3f kilometers\n\n", METER_TO_KM);
+
+    do {
+        
+        printf("Enter the distance in meters: ");
+        scanf("%f", &m);
+
+        conversion(m);  
+
+        printf("Do you want to convert another distance? (Y/N): ");
+        scanf(" %c", &choice);  
+
+    } while (choice == 'Y' || choice == 'y');
+
+    printf("Exiting program......\n");
+    return 0;
+}
