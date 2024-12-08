e2f5085b2b334270109faec27b1732c6bfd62114
Samiya Khan <khansamiya2005@gmail.com>
2024-12-08 11:13:50 +0500

    Update Q2.c

--- PF THEORY Assignment 03/Q2/Q2.c
+++ PF THEORY Assignment 03/Q2/Q2.c
@@ -1,41 +1,122 @@
 #include<stdio.h>
 #include<string.h>
 
-int main () {
+#define BALLS 12
 
-char slogans[3][50] = {"buy now", "save big", "limited offer"};
-
-int num_of_slogans = 3; 
-char character; 
-
-for (int i = 0; i < num_of_slogans; i++)
+struct Player 
 {
-    int processed[256] = {0}; 
+    char playerName[50];
+    int ballScores[BALLS];
+    int totalScore;
+};
 
-    for (int j = 0; slogans[i][j] != '\0'; j++)
+void playGame(struct Player *player);
+int validateScore(int score);
+struct Player findWinner(struct Player player1, struct Player player2);
+void displayMatchScoreboard(struct Player player1, struct Player player2);
+
+int main() 
+{
+    struct Player player1 = { .totalScore = 0 };
+    struct Player player2 = { .totalScore = 0 };
+
+    printf("Enter Player 1 name: ");
+    fgets(player1.playerName, 50, stdin);
+    player1.playerName[strcspn(player1.playerName, "\n")] = 0; 
+
+    printf("Enter Player 2 name: ");
+    fgets(player2.playerName, 50, stdin);
+    player2.playerName[strcspn(player2.playerName, "\n")] = 0;
+
+    printf("\n-- Player 1 Turn --\n");
+    playGame(&player1);
+
+    printf("\n-- Player 2 Turn --\n");
+    playGame(&player2);
+
+    printf("\n-- Match Summary --\n");
+    displayMatchScoreboard(player1, player2);
+
+    struct Player winner = findWinner(player1, player2);
+    
+    if (winner.totalScore != -1) 
     {
-        processed[slogans[i][j]]++; 
-    }
-
-    printf("For \"%s\" : {", slogans[i]);
-    int commas = 1; 
-
-    for (int k = 0; slogans[i][k] != '\0'; k++) 
+        printf("\nWinner: %s with a total score of %d!\n", winner.playerName, winner.totalScore);
+    } 
+    
+    else 
     {
-        character = slogans[i][k];
-        if (processed[(unsigned char)character] > 0) 
-        {
-            if (commas != 1) 
-            {
-                printf(", ");
-            }
-                printf("'%c': %d", character, processed[character]);
-                processed[character] = 0;
-                commas = 0;
-        }
+        printf("\nIt's a tie!\n");
     }
-        printf("}\n"); 
-}
 
     return 0;
+}
+
+void playGame(struct Player *player) 
+{
+    printf("Player: %s\n", player->playerName);
+    
+    for (int i = 0; i < BALLS; i++) 
+    {
+        int score;
+        printf("Enter score for ball %d: ", i + 1);
+        scanf("%d", &score);
+
+        if (validateScore(score)) 
+        {
+            player->ballScores[i] = score;
+            player->totalScore += score;
+        } 
+        
+        else 
+        {
+            player->ballScores[i] = 0; 
+            printf("Invalid score! Ball marked but no runs scored.\n");
+        }
+    }
+}
+
+int validateScore(int score) 
+{
+    return score >= 0 && score <= 6;
+}
+
+struct Player findWinner(struct Player player1, struct Player player2) 
+{
+    if (player1.totalScore > player2.totalScore) 
+    {
+        return player1;
+    } 
+    
+    else if (player2.totalScore > player1.totalScore) 
+    {
+        return player2;
+    } 
+    
+    else 
+    {
+        struct Player tie = { .totalScore = -1 }; 
+        return tie;
+    }
+}
+
+void displayMatchScoreboard(struct Player player1, struct Player player2) 
+{
+    printf("\nScoreboard for %s:\n", player1.playerName);
+    for (int i = 0; i < BALLS; i++)
+    {
+        printf("Ball %d: %d\n", i + 1, player1.ballScores[i]);
+    }
+    
+    printf("Total Score: %d\n", player1.totalScore);
+    printf("Average Score: %.2f\n", player1.totalScore / (float)BALLS);
+
+    printf("\nScoreboard for %s:\n", player2.playerName);
+    for (int i = 0; i < BALLS; i++) 
+    {
+        printf("Ball %d: %d\n", i + 1, player2.ballScores[i]);
+    }
+    
+    printf("Total Score: %d\n", player2.totalScore);
+    printf("Average Score: %.2f\n", player2.totalScore / (float)BALLS);
 }
